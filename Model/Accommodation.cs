1ff2addf381adc9d48e45b12206c2ef6f0df28d7
Valentina Gacic <valentinagacic1@gmail.com>
2024-03-07 12:23:47 +0100

    ispravka sintaksnih gresaka

---
+++ Model/Accommodation.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace BookingApp.Model
+{
+    public enum AccommodationType
+    {
+        apartment,
+        house,
+        hut
+    }
+    public class Accommodation
+    {
+        private int AccommodationId {  get; set; }
+        private int OwnerId {  get; set; }
+        private string Name { get; set; }
+        private AccommodationType Type { get; set; }
+        private Location Location { get; set; }
+        private int MaxGuests { get; set; }
+        private int MinReservationDays { get; set; }
+        private int CancellationDays { get; set; }
+        private List<string> Pictures { get; set; }
+
+
+        public Accommodation() 
+            {
+            }
+        public Accommodation(int, idAccommodation, int idOwner, string name, AccommodationType type, Location location int maxGuests, int minReservationDays, int cancellationDays)
+            {
+                this.AccommodationId = idAccommodation;
+                this.OwnerId = idOwner;
+                this.Name = name;
+                this.Type = type;
+                this.Location = location;
+                this.MinReservationDays = minReservationDays;
+                this.CancellationDays = cancellationDays;
+                this.Pictures = new List<string>();
+            }
+
+
+    }
+
+
+
+}
