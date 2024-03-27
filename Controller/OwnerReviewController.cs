13bdc6d92c63b78c0cf9903a9cc7b9ac2cd0705b
Valentina Gacic <valentinagacic1@gmail.com>
2024-03-27 11:06:51 +0100

    model, repository,servise,controller,csv za owner review

---
+++ Controller/OwnerReviewController.cs
@@ -0,0 +1,25 @@
+﻿using BookingApp.Model;
+using BookingApp.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Controller
+{
+    public class OwnerReviewController
+    {
+        private OwnerReviewService _ownerReviewService;
+
+        public OwnerReviewController(OwnerReviewService ownerReviewService)
+        {
+            _ownerReviewService = ownerReviewService;
+        }
+
+        public void RateOwner(OwnerReview ownerReview)
+        {
+            _ownerReviewService.RateOwner(ownerReview);
+        }
+    }
+}
---
+++ Model/OwnerReview.cs
@@ -0,0 +1,54 @@
+﻿using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BookingApp.Model
+{
+    public class OwnerReview : ISerializable
+    {
+
+        public int Id { get; set; }
+        public AccommodationReservation AccommodationReservation { get; set; }
+        public int Cleanliness { get; set; }
+        public int RuleAdherence { get; set; }
+        public string Comment { get; set; }
+
+        public List<string> Pictures { get; set; }
+
+
+        public OwnerReview() { }
+
+        public OwnerReview(AccommodationReservation accommodationReservation, int cleanliness, int ruleAdherence, string comment,List<string> pictures)
+        {
+
+            this.AccommodationReservation = accommodationReservation;
+            this.Cleanliness = cleanliness;
+            this.RuleAdherence = ruleAdherence;
+            this.Comment = comment;
+            this.Pictures = pictures;
+        }
+
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            AccommodationReservation = new AccommodationReservation() { Id = Convert.ToInt32(values[1]) };
+            Cleanliness = Convert.ToInt32(values[2]);
+            RuleAdherence = Convert.ToInt32(values[3]);
+            Comment = values[4];
+            Pictures = values[5].Split(",").ToList();
+        }
+
+        public string[] ToCSV()
+        {
+            string picturesString = string.Join(",", Pictures);
+            string[] csvValues = {Id.ToString(),AccommodationReservation.Id.ToString(),Cleanliness.ToString(),RuleAdherence.ToString(),Comment};
+            return csvValues;
+        }
+    }
+}
