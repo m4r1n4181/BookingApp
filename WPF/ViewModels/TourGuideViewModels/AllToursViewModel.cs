6652496bd806214f7dc2832ff39b2c24dabe825a
Helena Djordjevic <helendjordjevic@gmail.com>
2024-05-05 23:32:10 +0200

    zapocet i hci

--- WPF/ViewModels/TourGuideViewModels/AllToursViewModel.cs
+++ WPF/ViewModels/TourGuideViewModels/AllToursViewModel.cs
@@ -34,6 +34,7 @@ namespace BookingApp.View.ViewModels.TourGuideViewModels
         public RelayCommand AddCommand { get; set; }
         public RelayCommand TourStatisticsCommand { get; set; }
         public RelayCommand TutorialCommand { get; set; }
+        public RelayCommand SearchCommand { get; set; }
 
         public AllToursViewModel()
         {
@@ -44,6 +45,7 @@ namespace BookingApp.View.ViewModels.TourGuideViewModels
             AddCommand = new RelayCommand(AddTours_Click, CanExecuteAddToursClick);
             TourStatisticsCommand = new RelayCommand(TourStatistics_Click, CanExecuteTourStatisticsClick);
             TutorialCommand = new RelayCommand(Tutorial_Click, CanExecuteTutorialClick);
+            SearchCommand = new RelayCommand(Search_Click, CanExecuteSearchClick);
 
         }
 
@@ -64,12 +66,7 @@ namespace BookingApp.View.ViewModels.TourGuideViewModels
         public bool CanExecuteViewClick(object param)
         {
             return true;
-            // if (SelectedTour == null)
-            //{
-            // MessageBox.Show("Please select a tour.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            //  return false;
-            //}
-            //return true;
+         
         }
 
         public void ActiveTours_Click(object param)
@@ -83,12 +80,7 @@ namespace BookingApp.View.ViewModels.TourGuideViewModels
         public bool CanExecuteActiveToursClick(object param)
         {
             return true;
-            // if (SelectedTour == null)
-            //{
-            // MessageBox.Show("Please select a tour.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            //  return false;
-            //}
-            //return true;
+         
         }
 
         public void AddTours_Click(object param)
@@ -102,12 +94,7 @@ namespace BookingApp.View.ViewModels.TourGuideViewModels
         public bool CanExecuteAddToursClick(object param)
         {
             return true;
-            // if (SelectedTour == null)
-            //{
-            // MessageBox.Show("Please select a tour.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            //  return false;
-            //}
-            //return true;
+          
         }
 
         public void TourStatistics_Click(object param)
@@ -121,12 +108,7 @@ namespace BookingApp.View.ViewModels.TourGuideViewModels
         public bool CanExecuteTourStatisticsClick(object param)
         {
             return true;
-            // if (SelectedTour == null)
-            //{
-            // MessageBox.Show("Please select a tour.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            //  return false;
-            //}
-            //return true;
+         
         }
 
         public void Tutorial_Click(object param)
@@ -138,12 +120,21 @@ namespace BookingApp.View.ViewModels.TourGuideViewModels
         public bool CanExecuteTutorialClick(object param)
         {
             return true;
-            // if (SelectedTour == null)
-            //{
-            // MessageBox.Show("Please select a tour.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            //  return false;
-            //}
-            //return true;
+          
+        }
+
+        
+        public void Search_Click(object param)
+        {
+           
+         //treba mi logika za search kao ako ukucam koju god slovo izadju tura
+         //na tom pocetnom slovu
+
+        }
+        public bool CanExecuteSearchClick(object param)
+        {
+            return true;
+          
         }
 
 
