f9905e9c2ab57afedc6fb6a36021a1517b0a594e
Helena Djordjevic <helendjordjevic@gmail.com>
2024-06-04 18:34:56 +0200

    Zapocet tour request prozor za hci

--- WPF/View/TourGuideWindows/SearchTourRequests.xaml.cs
+++ WPF/View/TourGuideWindows/SearchTourRequests.xaml.cs
@@ -3,6 +3,7 @@ using BookingApp.Domain.Models;
 using BookingApp.DTO;
 using BookingApp.Model;
 using BookingApp.Model.Enums;
+using BookingApp.View;
 using BookingApp.WPF.ViewModels.TourGuideViewModels;
 using System;
 using System.Collections;
@@ -36,6 +37,71 @@ namespace BookingApp.WPF.View.TourGuideWindows
             this.DataContext = new SearchTourRequestsViewModel();
         
         }
+
+        private void LiveTour_Click(object sender, RoutedEventArgs e)
+        {
+            LiveTourView liveTourView = new LiveTourView();
+            liveTourView.Show();
+        }
+
+        private void AllFutureTours_Click(object sender, RoutedEventArgs e)
+        {
+            TourReservation tourReservation = new TourReservation();
+
+            FutureTours futureTours = new FutureTours();
+            futureTours.Show();
+        }
+
+        private void TourStatistics_Click(object sender, RoutedEventArgs e)
+        {
+
+            TourStatisticsOverview tourStatisticsOverview = new TourStatisticsOverview();
+            tourStatisticsOverview.Show();
+        }
+
+        private void TourGuideReviews_Click(object sender, RoutedEventArgs e)
+        {
+
+            TourGuideReviews tourGuideReviews = new TourGuideReviews();
+            tourGuideReviews.Show();
+        }
+        private void AllTours_Click(object sender, RoutedEventArgs e)
+        {
+
+            AllTours allTours = new AllTours();
+            allTours.Show();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Requests_Click(object sender, RoutedEventArgs e)
+        {
+            SearchTourRequests searchTourRequests = new SearchTourRequests();
+            searchTourRequests.Show();
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+
+            SignInForm signInForm = new SignInForm();
+            signInForm.Show();
+            //za sad me samo baca na signin, posle neka logika 
+        }
+
+        private void Profile_Click(object sender, RoutedEventArgs e)
+        {
+            GuideProfile guideProfile = new GuideProfile();
+            guideProfile.Show();
+        }
+
+        private void Tutorial_Click(object sender, RoutedEventArgs e)
+        {
+            //otvara prozor za tutorial 
+        }
+
 
     }
 }
